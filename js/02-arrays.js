3d6d7a1bcaeb4c2adf02990952b3f3737268ca5b
Jiuberto <111294204+Jiuberto@users.noreply.github.com>
2024-04-16 17:28:13 -0300

    arrayjs1

--- js/02-arrays.js
+++ js/02-arrays.js
@@ -1,5 +1,7 @@
 'use strict'
 
+/* vetor (array de uma dimensão) */
+
 //Declarando array
 const alunos = ["Midorya", "Todoroki", "Bakugo", "Uraraka"]
 
@@ -17,4 +19,12 @@ console.log(livro);
 let mensagem = `Já li ${livro[1]} e ${livro[6]} mas falta terminar ${livro[4]}`
 
 console.log(mensagem);
+
+/* Matriz (array com maisde uma dimensão) */
+const tecnologias = [
+    ['HTML5','CSS3','JS'],
+    ['PHP','SQL','NODE.JS']
+];
+
+console.log(tecnologias[0][1], tecnologias[1][0]);
 
