3d1015737ee95b7abbd942361b043e2b38055563
Jiuberto <111294204+Jiuberto@users.noreply.github.com>
2024-04-18 15:29:09 -0300

    op2.

--- js/operadores-logicos.js
+++ js/operadores-logicos.js
@@ -18,3 +18,21 @@ if (mediaFinal >= mediaMinima && faltas <= limiteDeFaltas) {
 }
 
 console.log(resultado);
+
+console.log("--------");
+
+let idade = 25
+
+//operador ou/or (||) obs: o simbolo | é chamao pipe
+if ( idade >= 18 || idade <= 12) {
+    console.log('Voceé um adulto ou criança');
+} else {'adolescente'
+    
+}
+
+let onibus = 2203; // 2203 ou 2019
+if (onibus == 2203 || onibus == 2019) {
+    console.log("embarque");
+} else {
+    console.log('aguarde');
+}
