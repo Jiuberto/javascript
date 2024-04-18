3fe044f4da1bc848b18f2cd73a1753c994313205
Jiuberto <111294204+Jiuberto@users.noreply.github.com>
2024-04-18 17:23:17 -0300

    loop2

--- js/06-loops.js
+++ js/06-loops.js
@@ -17,8 +17,36 @@ while (contador <= 5) {
 } */
 
 //loop for
-for (let i = 1; i <= 10; i++) {
+/* for (let i = 1; i <= 10; i++) {
     console.log('contador '+i);
-}
+} */
 
 /* obs: normalmente, variáveis de controloles de repetição são chamados de i,j,k */
+
+//loop para acessar array
+const alunos = ['Midorya', 'Uraraka', 'Bakugo','Todoroki', 'Kirishima','Iida']
+
+/* for (let i = 0; i < alunos.length; i++ ) {
+    console.log(alunos[i]);
+}
+
+console.log(alunos[0]); */
+
+//loop exclusivo do js para arrys: for/of
+for(const aluno of alunos){
+    console.log(aluno);
+}
+
+//loop exclusivo do js para objetos: for/in
+const livro = {
+    titulo: 'HTML5',
+    autor: 'Maujor',
+    site: 'https://maujor.com',
+    ano: 2016
+}
+
+console.log('-----------');
+
+for(const prop in livro){
+    console.log(livro[prop]);
+}
